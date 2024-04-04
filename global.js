c4164fe46e4005cef64a3549ae5cea4a59ff2a65
Pirince-kisele <156867185+Pirince-kisele@users.noreply.github.com>
2024-04-04 19:00:45 +0000

    global object

--- global.js
+++ global.js
@@ -1,12 +1,17 @@
 // Global object
 
 console.log(global);
-
-global.setTimeout(()=> {
+// set timeout is the function of the global object you can take of the global object in frount of the function it will be the same
+setTimeout(()=> {
   console.log('in the time out')
+  clearInterval(int)
 },3000);
 
 
   const int = setInterval(()=> {
     console.log('in the interval')
   },1000);
+
+
+console.log(__direname); // this gives us the absolut path of the file
+console.log(__filename);// this gives us the absolut path of the file an the name of the file
